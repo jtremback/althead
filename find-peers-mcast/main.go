6efae0697e4db5a568d40d10fb11f8a356b0d595
Jehan <jehan@Jehans-MacBook.local>
2016-09-26 16:52:38 -0700

    last commit for fastd version

--- find-peers-mcast/main.go
+++ find-peers-mcast/main.go
@@ -3,13 +3,60 @@ package findPeersMCast
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net"
+	"os"
+	"strings"
 )
+
+func setup(
+	iface *net.Interface,
+	mcastPort int,
+	secret string,
+	pubkey string,
+) error {
+	conf := fmt.Sprintf(`
+bind %v:%v;
+mode tap;
+method "xsalsa20-poly1305";
+mtu 1426;
+secret "%v";
+interface "%k";
+
+include peers from "/tmp/althea/fastd_peers";
+	`, iface, mcastPort, secret)
+
+	confFile, err := ioutil.TempFile("", "althead_fastd_conf")
+	if err != nil {
+		return err
+	}
+
+	defer os.Remove(confFile.Name())
+
+	_, err = confFile.Write([]byte(conf))
+	if err != nil {
+		return err
+	}
+
+	err = confFile.Close()
+	if err != nil {
+		return err
+	}
+
+	peersDir, err := ioutil.TempDir("", "althead_fastd_peers")
+	if err != nil {
+		return err
+	}
+
+}
 
 func Advertise(
 	iface *net.Interface,
 	mcastPort int,
+	secret string,
+	pubkey string,
 ) error {
+
 	conn, err := net.ListenMulticastUDP(
 		"udp6",
 		iface,
@@ -24,12 +71,15 @@ func Advertise(
 	}
 
 	for {
-		b := make([]byte, 12)
+		b := make([]byte, 64)
 		_, addr, err := conn.ReadFromUDP(b)
 		if err != nil {
 			return err
 		}
-		if string(b) == "althea_hello" {
+
+		msg := strings.Split(string(b), " ")
+
+		if msg[0] == "althea_hello" {
 			conn, err := net.DialUDP(
 				"udp6",
 				nil,
@@ -39,7 +89,20 @@ func Advertise(
 				return err
 			}
 
-			conn.Write([]byte("althea_ihu"))
+			// write peer file
+			peerFilename := peersDir + msg[1]
+			peerConf := []byte(fmt.Sprintf(`key %v;`, msg[1]))
+			err = ioutil.WriteFile(peerFilename, peerConf, 0600)
+			if err != nil {
+				return err
+			}
+
+			defer os.Remove(peerFilename)
+
+			conn.Write([]byte(
+				"althea_ihu " +
+					"",
+			))
 
 			conn.Close()
 
@@ -90,7 +153,9 @@ func QueryPeers(
 
 	defer conn.Close()
 
-	conn.WriteToUDP([]byte("althea_hello"), &net.UDPAddr{
+	conn.WriteToUDP([]byte("althea_hello "+
+		"b023b49ef699da32cef9046434ab5f73d0ea7b8ee4e4bb31b8f4f2e27d138e54\n",
+	), &net.UDPAddr{
 		IP:   net.ParseIP("ff02::1"),
 		Port: mCastPort,
 		Zone: iface.Name,

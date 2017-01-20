e58d97b3de2093306085504b4543169251d9fedb
Jehan <jehan@Jehans-MacBook.local>
2017-01-19 22:21:36 -0800

    refactor for testability

--- neighbor-api/main.go
+++ neighbor-api/main.go
@@ -14,22 +14,25 @@ import (
 type NeighborAPI struct {
 	Neighbors map[[ed25519.PublicKeySize]byte]*types.Neighbor
 	Account   *types.Account
+	Network   interface {
+		SendUDP(*net.UDPAddr, string) error
+	}
 }
 
 // McastListen listens on the multicast UDP address on a given interface. When it gets
 // an scrooge_hello packet, it calls HelloHandler and sends an scrooge_hello packet
 // to the ControlAddress of the Neighbor.
-func (a *NeighborAPI) McastListen(
-	mcastPort int,
+func (self *NeighborAPI) McastListen(
+	port int,
 	iface *net.Interface,
-	cb func(*types.Neighbor, error),
+	cb func(error),
 ) error {
 	conn, err := net.ListenMulticastUDP(
 		"udp6",
 		iface,
 		&net.UDPAddr{
 			IP:   net.ParseIP("ff02::1"),
-			Port: mcastPort,
+			Port: port,
 			Zone: iface.Name,
 		},
 	)
@@ -41,83 +44,30 @@ func (a *NeighborAPI) McastListen(
 		var b []byte
 		_, _, err := conn.ReadFromUDP(b)
 		if err != nil {
-			cb(nil, err)
+			cb(err)
 			continue
 		}
-
-		msg := strings.Split(string(b), " ")
-
-		log.Println("received: " + string(b))
-
-		if msg[0] == "scrooge_hello" {
-			neighbor, err := a.HelloHandler(msg)
-			if err != nil {
-				cb(nil, err)
-				continue
-			}
-
-			addr, err := net.ResolveUDPAddr("udp6", neighbor.ControlAddress)
-			if err != nil {
-				cb(nil, err)
-				continue
-			}
-
-			err = sendUDP(addr, serialization.FmtHello(a.Account))
-			if err != nil {
-				cb(nil, err)
-				continue
-			}
-
-			cb(neighbor, nil)
-		} else {
-			cb(nil, errors.New("unrecognized message type"))
-			continue
-		}
+		cb(self.Handlers(b))
 	}
 
 	return nil
 }
 
-func (a *NeighborAPI) createTunnel() {
-
-}
-
-func (a *NeighborAPI) HelloHandler(msg []string) (*types.Neighbor, error) {
-	helloMessage, err := serialization.ParseHello(msg)
-	if err != nil {
-		return nil, err
-	}
-
-	neighbor := a.Neighbors[helloMessage.PublicKey]
-	if neighbor == nil {
-		neighbor = &types.Neighbor{
-			PublicKey: helloMessage.PublicKey,
-		}
-	}
-
-	if neighbor.Seqnum > helloMessage.Seqnum {
-		return nil, errors.New("sequence number too low")
-	}
-
-	neighbor.Seqnum = helloMessage.Seqnum
-	neighbor.ControlAddress = helloMessage.ControlAddress
-
-	return neighbor, nil
-}
-
 // ControlListen listens on the ControlAddress and passes received messages to
 // the appropriate handler function.
-func (a *NeighborAPI) ControlListen(
-	cb func(*types.Neighbor, error),
+func (self *NeighborAPI) ControlListen(
+	port int,
+	iface *net.Interface,
+	cb func(error),
 ) error {
-	addr, err := net.ResolveUDPAddr("udp6", a.Account.ControlAddress)
+	addr, err := net.ResolveUDPAddr("udp6", self.Account.ControlAddress)
 	if err != nil {
 		return err
 	}
 
 	conn, err := net.ListenUDP("udp6", addr)
 	if err != nil {
-		cb(nil, err)
+		return err
 	}
 
 	defer conn.Close()
@@ -126,61 +76,91 @@ func (a *NeighborAPI) ControlListen(
 		var b []byte
 		_, _, err := conn.ReadFromUDP(b)
 		if err != nil {
-			cb(nil, err)
+			cb(err)
 			continue
 		}
+		cb(self.Handlers(b))
+	}
 
-		msg := strings.Split(string(b), " ")
+	return nil
+}
 
-		log.Println("received: " + string(b))
+func (self *NeighborAPI) Handlers(b []byte) error {
+	msg := strings.Split(string(b), " ")
 
-		if msg[0] == "scrooge_hello" {
-			_, err := a.HelloHandler(msg)
-			if err != nil {
-				cb(nil, err)
-				continue
-			}
-		} else {
-			cb(nil, errors.New("unrecognized message type"))
-			continue
+	log.Println("received: " + string(b))
+
+	if msg[0] == "scrooge_hello" {
+		return self.HelloHandler(msg)
+	} else {
+		return errors.New("unrecognized message type")
+	}
+}
+
+func (self *NeighborAPI) HelloHandler(msg []string) error {
+	helloMessage, err := serialization.ParseHello(msg)
+	if err != nil {
+		return err
+	}
+
+	neighbor := self.Neighbors[helloMessage.PublicKey]
+	if neighbor == nil {
+		neighbor = &types.Neighbor{
+			PublicKey: helloMessage.PublicKey,
 		}
 	}
 
+	if neighbor.Seqnum > helloMessage.Seqnum {
+		return errors.New("sequence number too low")
+	}
+
+	neighbor.Seqnum = helloMessage.Seqnum
+	neighbor.ControlAddress = helloMessage.ControlAddress
+
+	addr, err := net.ResolveUDPAddr("udp6", neighbor.ControlAddress)
+	if err != nil {
+		return err
+	}
+
+	err = self.Network.SendUDP(addr, serialization.FmtHello(self.Account))
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // McastHello sends an scrooge_hello packet to the multicast UDP address on a given interface.
-func (a *NeighborAPI) McastHello(
+func (self *NeighborAPI) McastHello(
 	mCastPort int,
 	iface *net.Interface,
 	cb func(*types.Neighbor, error),
 ) error {
-	err := sendUDP(&net.UDPAddr{
+	err := self.Network.SendUDP(&net.UDPAddr{
 		IP:   net.ParseIP("ff02::1"),
 		Port: mCastPort,
 		Zone: iface.Name,
-	}, serialization.FmtHello(a.Account))
+	}, serialization.FmtHello(self.Account))
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func sendUDP(
-	addr *net.UDPAddr,
-	s string,
-) error {
-	conn, err := net.DialUDP(
-		"udp6",
-		nil,
-		addr,
-	)
-	defer conn.Close()
-	if err != nil {
-		return err
-	}
+// func (self *NeighborAPI) sendUDP(
+// 	addr *net.UDPAddr,
+// 	s string,
+// ) error {
+// 	conn, err := net.DialUDP(
+// 		"udp6",
+// 		nil,
+// 		addr,
+// 	)
+// 	defer conn.Close()
+// 	if err != nil {
+// 		return err
+// 	}
 
-	conn.Write([]byte(s))
-	log.Println("sent: " + s)
-	return nil
-}
+// 	conn.Write([]byte(s))
+// 	log.Println("sent: " + s)
+// 	return nil
+// }

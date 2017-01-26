9b2e028a7ba32b19b96242771781c8a5b3c5cf5c
Jehan <jehan.tremback@gmail.com>
2017-01-25 23:30:45 -0800

    cleanup

--- serialization/serialization.go
+++ serialization/serialization.go
@@ -12,7 +12,7 @@ import (
 	"github.com/jtremback/scrooge/types"
 )
 
-// scrooge_hello <publicKey> <control address> <seqnum> <signature>
+// scrooge_hello<_confirm> <publicKey> <control address> <seqnum> <signature>
 func FmtHello(
 	account *types.Account,
 	controlAddress string,
@@ -66,74 +66,7 @@ func ParseHello(msg []string) (*types.HelloMessage, error) {
 	return h, nil
 }
 
-// scrooge_hello_confirm <publicKey> <control address> <seqnum> <signature>
-// func FmtHelloConfirm(
-// 	account *types.Account,
-// 	controlAddress string,
-// ) string {
-// 	msg := types.HelloConfirmMessage{
-// 		MessageMetadata: types.MessageMetadata{
-// 			Seqnum:    account.Seqnum,
-// 			PublicKey: account.PublicKey,
-// 		},
-// 		ControlAddress: controlAddress,
-// 	}
-
-// 	s := fmt.Sprintf(
-// 		"scrooge_hello_confirm %v %v %v",
-// 		base64.StdEncoding.EncodeToString(msg.PublicKey[:]),
-// 		msg.ControlAddress,
-// 		msg.Seqnum,
-// 	)
-
-// 	sig := ed25519.Sign(&account.PrivateKey, []byte(s))
-
-// 	return s + " " + base64.StdEncoding.EncodeToString(sig[:])
-// }
-
-// func FmtHelloConfirm(
-// 	account *types.Account,
-// 	controlAddress string,
-// ) (string, error) {
-// 	msg := types.HelloMessage{
-// 		MessageMetadata: types.MessageMetadata{
-// 			Seqnum:    account.Seqnum,
-// 			PublicKey: account.PublicKey,
-// 		},
-// 		ControlAddress: controlAddress,
-// 	}
-
-// 	if len(msg.ControlAddress) == 0 {
-// 		return "", errors.New("invalid ControlAddress")
-// 	}
-
-// 	s := fmt.Sprintf(
-// 		"scrooge_hello_confirm %v %v %v",
-// 		base64.StdEncoding.EncodeToString(msg.PublicKey[:]),
-// 		msg.ControlAddress,
-// 		msg.Seqnum,
-// 	)
-
-// 	sig := ed25519.Sign(&account.PrivateKey, []byte(s))
-
-// 	return s + " " + base64.StdEncoding.EncodeToString(sig[:]), nil
-// }
-
-// func ParseHelloConfirm(msg []string) (*types.HelloConfirmMessage, error) {
-// 	messageMetadata, err := verifyMessage(msg)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	h := &types.HelloConfirmMessage{
-// 		MessageMetadata: *messageMetadata,
-// 		ControlAddress:  msg[2],
-// 	}
-
-// 	return h, nil
-// }
-
-// scrooge_tunnel <publicKey> <tunnel publicKey> <tunnel endpoint> <seq num> <signature>
+// scrooge_tunnel<_confirm> <publicKey> <tunnel publicKey> <tunnel endpoint> <seq num> <signature>
 func FmtTunnel(
 	account *types.Account,
 	neighbor *types.Neighbor,
@@ -186,48 +119,6 @@ func ParseTunnel(msg []string) (*types.TunnelMessage, error) {
 	return m, nil
 }
 
-// scrooge_tunnel_confirm <publicKey> <tunnel publicKey> <tunnel endpoint> <seq num> <signature>
-// func FmtTunnelConfirm(
-// 	account *types.Account,
-// 	neighbor *types.Neighbor,
-// 	iface *net.Interface,
-// ) string {
-// 	m := types.TunnelConfirmMessage{
-// 		MessageMetadata: types.MessageMetadata{
-// 			PublicKey: account.PublicKey,
-// 			Seqnum:    account.Seqnum,
-// 		},
-// 		TunnelEndpoint:  neighbor.Tunnel.Endpoint,
-// 		TunnelPublicKey: neighbor.Tunnel.PublicKey,
-// 	}
-
-// 	msg := fmt.Sprintf(
-// 		"scrooge_tunnel_confirm %v %v %v",
-// 		base64.StdEncoding.EncodeToString(m.PublicKey[:]),
-// 		m.TunnelPublicKey,
-// 		m.TunnelEndpoint,
-// 		m.Seqnum,
-// 	)
-
-// 	sig := ed25519.Sign(&account.PrivateKey, []byte(msg))
-
-// 	return msg + " " + base64.StdEncoding.EncodeToString(sig[:])
-// }
-
-// func ParseTunnelConfirm(msg []string) (*types.TunnelConfirmMessage, error) {
-// 	messageMetadata, err := verifyMessage(msg)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	m := &types.TunnelConfirmMessage{
-// 		MessageMetadata: *messageMetadata,
-// 		TunnelPublicKey: msg[2],
-// 		TunnelEndpoint:  msg[3],
-// 	}
-
-// 	return m, nil
-// }
 
 func verifyMessage(msg []string) (*types.MessageMetadata, error) {
 	pk, err := base64.StdEncoding.DecodeString(msg[1])

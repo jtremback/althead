e04a43f926ac93ebc19314da4fa36e9e1b77690f
Jehan <jehan.tremback@gmail.com>
2017-02-10 17:14:35 -0800

    experiments in smoketest

---
+++ test/smoketest.sh
@@ -0,0 +1,9 @@
+#!bash
+
+export GOPATH=/vagrant
+
+# sudo modprobe dummy numdummies=1
+# sudo ip addr add 10.0.1.2/24 dev "$(sudo brctl show | awk '/dummy0/ { print $1 }')"
+/usr/local/go/bin/go build && /usr/bin/python ./test/smoketest.py &
+# sleep 555
+# sudo brctl show
